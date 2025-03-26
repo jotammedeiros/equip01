9fcdc6a4fea702194066ac2b230da6db526921f1
jotammedeiros <jopam.fsa@gmail.com>
2025-03-26 10:56:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// CÓDIGO PARA FAZER MENU HAMBURGUER SIMPLES :)
+function clickMenu () {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
